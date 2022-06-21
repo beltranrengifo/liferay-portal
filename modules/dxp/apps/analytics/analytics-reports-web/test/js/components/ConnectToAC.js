80f7a3650ae632a08dfab00dabade0cc8d9e0265
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-06-21 08:07:07 +0200

    LPS-156018 More tests

--- modules/dxp/apps/analytics/analytics-reports-web/test/js/components/ConnectToAC.js
+++ modules/dxp/apps/analytics/analytics-reports-web/test/js/components/ConnectToAC.js
@@ -9,34 +9,28 @@
  * distribution rights of the Software.
  */
 
-import {render} from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import React from 'react';
 
 import ConnectToAC from '../../../src/main/resources/META-INF/resources/js/components/ConnectToAC';
 
 import '@testing-library/jest-dom/extend-expect';
 
+const testProps = {
+	analyticsCloudTrialURL: 'https://localhost/',
+	analyticsURL: 'https://localhost/',
+	hideAnalyticsReportsPanelURL: 'https://localhost/',
+	isAnalyticsConnected: true,
+	pathToAssets: '/',
+};
+
+const renderComponent = (props) => {
+	return render(<ConnectToAC {...testProps} {...props} />);
+};
+
 describe('ConnectToAC', () => {
 	it('renders a link to analytics cloud in order to connect with our site when analytics is connected', () => {
-		const testProps = {
-			analyticsCloudTrialURL: 'https://localhost/',
-			analyticsURL: 'https://localhost/',
-			hideAnalyticsReportsPanelURL: 'https://localhost/',
-			isAnalyticsConnected: true,
-			pathToAssets: '/',
-		};
-
-		const {container, getByText} = render(
-			<ConnectToAC
-				analyticsCloudTrialURL={testProps.analyticsCloudTrialURL}
-				analyticsURL={testProps.analyticsURL}
-				hideAnalyticsReportsPanelURL={
-					testProps.hideAnalyticsReportsPanelURL
-				}
-				isAnalyticsConnected={testProps.isAnalyticsConnected}
-				pathToAssets={testProps.pathToAssets}
-			/>
-		);
+		const {container, getByText} = renderComponent();
 
 		expect(getByText('sync-to-analytics-cloud')).toBeInTheDocument();
 		expect(getByText('open-analytics-cloud')).toBeInTheDocument();
@@ -47,25 +41,9 @@ describe('ConnectToAC', () => {
 	});
 
 	it('renders a link to start free trial of AC when analytics is not connected', () => {
-		const testProps = {
-			analyticsCloudTrialURL: 'https://localhost/',
-			analyticsURL: 'https://localhost/',
-			hideAnalyticsReportsPanelURL: 'https://localhost/',
+		const {container, getByText} = renderComponent({
 			isAnalyticsConnected: false,
-			pathToAssets: '/',
-		};
-
-		const {container, getByText} = render(
-			<ConnectToAC
-				analyticsCloudTrialURL={testProps.analyticsCloudTrialURL}
-				analyticsURL={testProps.analyticsURL}
-				hideAnalyticsReportsPanelURL={
-					testProps.hideAnalyticsReportsPanelURL
-				}
-				isAnalyticsConnected={testProps.isAnalyticsConnected}
-				pathToAssets={testProps.pathToAssets}
-			/>
-		);
+		});
 
 		expect(
 			getByText('connect-to-liferay-analytics-cloud')
@@ -77,5 +55,50 @@ describe('ConnectToAC', () => {
 
 		expect(link[0].href).toBe(testProps.analyticsCloudTrialURL);
 		expect(link[1].href).toBe('');
+	});
+});
+
+describe('ConnectToAC component allows user to hide the module', () => {
+	beforeEach(() => {
+		window.submitForm = jest.fn();
+		document.hrefFm = (
+			<form
+				action="#"
+				aria-hidden="true"
+				className="hide"
+				id="hrefFm"
+				method="post"
+				name="hrefFm"
+			>
+				<span></span>
+
+				<button hidden="" type="submit">
+					Hidden
+				</button>
+			</form>
+		);
+	});
+
+	it('by rendering a link which will submit a form', () => {
+		const {getByText} = renderComponent({
+			isAnalyticsConnected: false,
+		});
+
+		const discardModuleLink = getByText('do-not-show-me-this-again');
+		expect(discardModuleLink).toBeInTheDocument();
+
+		fireEvent.click(discardModuleLink);
+		expect(window.submitForm).toHaveBeenCalled();
+	});
+
+	it('by rendering a link which will not submit the form if any param is missing', () => {
+		document.hrefFm = null;
+
+		const {getByText} = renderComponent({
+			isAnalyticsConnected: false,
+		});
+
+		fireEvent.click(getByText('do-not-show-me-this-again'));
+		expect(window.submitForm).not.toHaveBeenCalled();
 	});
 });
